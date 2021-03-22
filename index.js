07755c0a8eb4414468c1e164c65ace469aea9901
Layla Armstrong <larmstrong196@noscihigh.org>
2021-03-22 19:57:53 +0000

    Checkpoint

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /*
 PROMPT ONE
 Declare a function and name it add
-
 This function will take two parameters called 
 num1 & num2.
-
 add should return the sum of these two parameters
 */
-
+function add(){
+  return (num1 + num2);
+}
 
 
 
@@ -21,7 +21,7 @@ Use a function(PROMPT TWO)
 Declare a function called printer
 The printer function should print to the console no return or parameter needed
 */
-
+function printer()
 
 
 
@@ -37,11 +37,11 @@ Using Arguments(PROMPT THREE)
 
 Declare a variable called myObject, and assign it the value 
 of an object
-
+//var myObject
 Declare a function called addKeys
-
+//function addKeys()
 This function should take one parameter called object
-
+//return (object - 1)
 This function will add two keys named firstName and lastName 
 to the given paramater
 
